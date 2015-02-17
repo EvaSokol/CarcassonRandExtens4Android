8359210be931562990c9af202174a371804d8a93
EvaSokol <esokolyanskaya@lohika.com>
2015-02-17 09:22:13 +0200

    Two simple layouts created

---
+++ CarcassonRandomExtensions/src/com/example/carcassonrandomextensions/IncludingExtensions.java
@@ -0,0 +1,16 @@
+package com.example.carcassonrandomextensions;
+
+import com.example.caracassonrandomextensions.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class IncludingExtensions extends Activity {
+	
+	@Override
+	  protected void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.including_extensions);
+	  }
+
+}
